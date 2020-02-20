9678ffb02762ab01c989be31a905fc40f17bdaf7
doublejan <954464727@qq.com>
2020-02-20 22:20:43 +0800

    update: 添加nginx代理缓存状态头信息

--- server/index.js
+++ server/index.js
@@ -7,7 +7,15 @@ const server = http.createServer((req, res) => {
 
   // 跨域
   res.setHeader("Access-Control-Allow-Origin", '*');
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Cache-Control,X-Real-IP,Host");
+
+  // 允许的头信息和自定义头信息
+  // Content-type: 指定返回的类型为json
+  // Cache-Control: 指定允许使用客户端缓存
+  // X-Real-IP: 由于后端使用了nginx代理，将这个头由nginx填入真实的请求客户端ip带给node服务端
+  // Host: 代理主机名
+  // Nginx-Cache: nginx的代理缓存状态
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Cache-Control,X-Real-IP,Host,Nginx-Cache");
+  
   res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, OPTIONS, PATCH");
 
   // utf8解决中文乱码问题
